66221db607fa206603419afdb149aa640a9ad055
ttd <33125649+trytodupe@users.noreply.github.com>
2024-11-13 23:28:40 +0800

    Update ActivityBilling.java

--- app/src/github/java/eu/faircode/email/ActivityBilling.java
+++ app/src/github/java/eu/faircode/email/ActivityBilling.java
@@ -256,10 +256,11 @@ public class ActivityBilling extends ActivityBase implements
     }
 
     static boolean isPro(Context context) {
-        if (BuildConfig.DEBUG && false)
-            return true;
-        return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean("pro", false);
+        return true;
+        // if (BuildConfig.DEBUG && false)
+        //     return true;
+        // return PreferenceManager.getDefaultSharedPreferences(context)
+        //         .getBoolean("pro", false);
     }
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
